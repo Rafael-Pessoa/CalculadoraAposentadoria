44474ad5b926d0c33f2847670d0408ee32e94a72
Ogami_Raufa <brokenzero67@gmail.com>
2021-09-11 22:21:39 -0300

    Versão final - viewbinding
    Implementação - ViewBinding.

--- app/src/main/java/br/com/alura/calculadoraaposentadoria/MainActivity.kt
+++ app/src/main/java/br/com/alura/calculadoraaposentadoria/MainActivity.kt
@@ -2,32 +2,31 @@ package br.com.alura.calculadoraaposentadoria
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.*
-
+import br.com.alura.calculadoraaposentadoria.databinding.ActivityMainBinding
 
 class MainActivity:Activity(){
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        setContentView(R.layout.activity_main)
+        val MainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(MainBinding.root)
         super.onCreate(savedInstanceState)
-        val spn_sexo = findViewById<Spinner>(R.id.spn_sexo)
         var resultado = 0
-        val txt_idade = findViewById<EditText>(R.id.txt_idade)
-        val btn_calcular =findViewById<Button>(R.id.btn_calcular)
-        spn_sexo.adapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item, listOf("Masculino", "Feminino"))
-        var sexo = spn_sexo.selectedItem as String
-        var txt_aposentadoria =findViewById<TextView>(R.id.txt_aposentadoria)
 
-        btn_calcular.setOnClickListener {
+        //Spinner:
+        MainBinding.spnSexo.adapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item, listOf("Masculino", "Feminino"))
+        var sexo = MainBinding.spnSexo.selectedItem as String
 
-
-
+        //Comando Botão:
+        MainBinding.btnCalcular.setOnClickListener {
             if (sexo == "Masculino"){
-                resultado = 65 - txt_idade.text.toString().toInt()
+                resultado = 65 - MainBinding.txtIdade.text.toString().toInt()
 
             }else{
-                resultado = 60- txt_idade.text.toString().toInt()
+                resultado = 60 - MainBinding.txtIdade.text.toString().toInt()
             }
-            txt_aposentadoria.text="Faltam $resultado anos, para você se aposentar"
+            MainBinding.txtAposentadoria.text="Faltam $resultado anos, para você se aposentar"
         }
 
     }
